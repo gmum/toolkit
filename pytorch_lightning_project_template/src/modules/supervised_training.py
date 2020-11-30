b6567a0c64c0130982f4a275dbaffb595461abb1
wDaniec <andrzejZdobywca@gmail.com>
2020-12-01 00:06:38 +0100

    fixed _acuracy function for newer pytorch versions

--- pytorch_lightning_project_template/src/modules/supervised_training.py
+++ pytorch_lightning_project_template/src/modules/supervised_training.py
@@ -73,10 +73,9 @@ class SupervisedLearning(LightningModule):
             _, pred = output.topk(maxk, 1, True, True)
             pred = pred.t()
             correct = pred.eq(target.view(1, -1).expand_as(pred))
-
             res = []
             for k in topk:
-                correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+                correct_k = correct[:k].flatten().float().sum(0, keepdim=True)
                 res.append(correct_k.mul_(100.0 / batch_size))
             return res
 
